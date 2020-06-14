33a02807468dff8bcd0345a32de3a213ee4cb0d1
raslake <xguowater@gmail.com>
2020-06-14 20:47:02 +0800

    week01 assignment

---
+++ Week01/week01-2.py
@@ -0,0 +1,8 @@
+###python
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val: l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
